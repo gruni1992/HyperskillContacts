839e8f8dd30ff70514115963bbc84af82c3c5407
Tobias Grunwald <tobiasgrunwald92@gmail.com>
2020-06-27 11:55:24 +0200

    Added guard statement for phoneNumber initialization

--- Contacts/task/src/contacts/Record.java
+++ Contacts/task/src/contacts/Record.java
@@ -1,13 +1,24 @@
 package contacts;
 
+import contacts.exception.WrongNumberFormatException;
+
 public class Record {
-    String firstName;
-    String lastName;
-    String phoneNumber;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
 
     public Record(String firstName, String lastName, String phoneNumber) {
+        if (!isValidNumberFormat(phoneNumber)) {
+            throw new WrongNumberFormatException();
+        }
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
     }
+
+    private boolean isValidNumberFormat(String phoneNumber) {
+        //TODO
+        return false;
+    }
+
 }
