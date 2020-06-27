bd97eb61cdc8cb222069fa22efba32323344dc8e
Tobias Grunwald <tobiasgrunwald92@gmail.com>
2020-06-27 11:54:59 +0200

    added WrongNumberFormatException.java

---
+++ Contacts/task/src/contacts/exception/WrongNumberFormatException.java
@@ -0,0 +1,7 @@
+package contacts.exception;
+
+public class WrongNumberFormatException extends RuntimeException{
+    public WrongNumberFormatException() {
+        super("Wrong number Format!");
+    }
+}
